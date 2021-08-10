27c1b1dd1cb3e35d14adda4f6e403e51e19f3893
4neesc0des <aneeskuar@gmail.com>
2021-08-10 10:58:43 +0530

    final upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/users", userRouter);
 
 // listen server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Server is running on port ${PORT}`);
